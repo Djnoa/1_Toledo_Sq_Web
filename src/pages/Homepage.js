7a3d517afd7a86ed9d5845ebb45c65efe64cfbb3
Djnoa <dominicnoakes@icloud.com>
2024-01-01 14:06:03 +0100

    ;l

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,7 +5,19 @@ import './Homepage.css';
 import NavBar from '../components/navibar.js';
 import '../styles/main.scss'
 import Footer from '../components/footer.js';
-import { headerImagePaths, pongImage } from '../imagePaths.config.js'
+
+// Header images
+import headerImage1 from '../assets/header-img.jpg';
+import headerImage2 from '../assets/header-img2.jpg';
+import headerImage3 from '../assets/header-img3.jpg';
+import headerImage4 from '../assets/header-img4.jpg';
+import headerImage5 from '../assets/header-img5.jpg';
+import headerImage6 from '../assets/header-img6.jpg';
+import headerImage7 from '../assets/header-img7.jpg';
+
+// Other images
+import pongImage from './assets/pong.jpg';
+
 
 const headerImages = [
     headerImage1, 
